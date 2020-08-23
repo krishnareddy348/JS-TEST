e4dcf977dda94a614324fd632a8f67ff100f399d
Krishna Reddy <krishnareddy348@gmail.com>
2020-08-23 16:02:47 +0530

    Operators practice

---
+++ Operators.js
@@ -0,0 +1,13 @@
+var myName = "krishna";
+var age = 27;
+var height = 5.11;
+var email = "krishnareddy348@gmail.com";
+var DOB = new Date('jan-20-1993');
+
+
+
+console.log("Name: " + myName);
+console.log("Age: " + age);
+console.log("Height: " + height);
+console.log("Email: " + email);
+console.log("DOB: " + DOB);
---
+++ Var.js
@@ -0,0 +1,16 @@
+var K;
+var R;
+
+K = 74;
+R = 33;
+
+console.log("Add: " + (K+R));
+console.log("Sub: " + (K-R));
+console.log("Mul: " + (K*R));
+console.log("Div: " + (K/R));
+console.log("Mod: " + (K%R));
+
+
+var X = 24;
+
+console.log("Inc: " + (X++));
